20eaa964536258e54b06ebad5e25850504dc241c
pasberth <pasberth@gmail.com>
2012-02-28 06:33:02 +0900

    added tests

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -5,89 +5,133 @@ describe "when using for Enumerable" do
   let(:langs) { %w[c++ lisp] }
   let(:suffix) { "er" }
   
-  context "langs.map &:upcase.of(:+, suffix)" do
-    let(:result) { langs.map &:upcase.of(:+, suffix) }
-    let(:expected_result) { langs.map { |lang| (lang + suffix).upcase } }
-    subject { result }
-    it { should == expected_result }
-  end
+  describe "Symbol#of" do
   
-  context "langs.map &:upcase.and(:+, suffix)" do
-    let(:result) { langs.map &:upcase.and(:+, suffix) }
-    let(:expected_result) { langs.map { |lang| lang.upcase + suffix } }
-    subject { result }
-    it { should == expected_result }
-  end
-  
-  context "langs.map &:push.to(*receivers)" do
-    let(:receivers) { Array.new(3) { [] } }
-    let(:destructed_receivers) { Array.new(3) { langs } }
-    let(:result) { langs.map &:push.to(*receivers) }
-    let(:expected_result) { langs }
-    before { result }
-
-    describe "receivers" do
-      subject { receivers }
-      it { should == destructed_receivers }
+    context "langs.map &:upcase.of(:+, suffix)" do
+      let(:result) { langs.map &:upcase.of(:+, suffix) }
+      let(:expected_result) { langs.map { |lang| (lang + suffix).upcase } }
+      subject { result }
+      it { should == expected_result }
     end
-    
-    describe "result" do
+
+    context "langs.map &:upcase.of(:+, suffix).and(:+, 's')" do
+      let(:result) { langs.map &:upcase.of(:+, suffix).and(:+, 's') }
+      let(:expected_result) { langs.map { |lang| (lang + suffix).upcase + 's' } }
       subject { result }
       it { should == expected_result }
     end
   end
 
-  context "llangs.map &:capitalize.and_push.to(*receivers)" do
-    let(:receivers) { Array.new(3) { [] } }
-    let(:destructed_receivers) { Array.new(3) { langs.map { |lang| lang.capitalize } } }
-    let(:result) { langs.map &:capitalize.and_push.to(*receivers) }
-    let(:expected_result) { langs.map &:capitalize }
-    before { result }
-    
-    describe "receivers" do
-      subject { receivers }
-      it { should == destructed_receivers }
-    end
-    
-    describe "result" do
-      subject { result }
-      it { should == expected_result }
-    end
-  end
+  describe "Symbol#and" do
 
-  context "langs.map &:capitalize.of_push.to(*receivers)" do
-    let(:receivers) { Array.new(3) { [] } }
-    let(:destructed_receivers) { Array.new(3) { langs } }
-    let(:result) { langs.map &:capitalize.of_push.to(*receivers) }
-    let(:expected_result) { langs.map &:capitalize }
-    before { result }
-    
-    describe "receivers" do
-      subject { receivers }
-      it { should == destructed_receivers }
-    end
-    
-    describe "result" do
+    context "langs.map &:upcase.and(:+, suffix)" do
+      let(:result) { langs.map &:upcase.and(:+, suffix) }
+      let(:expected_result) { langs.map { |lang| lang.upcase + suffix } }
       subject { result }
       it { should == expected_result }
     end
   end
   
+  describe "Symbol#to" do
   
-  context "langs.map &:+.in(receiver)" do
-    let(:receiver) { "The " }
-    let(:result) { langs.map &:+.in(receiver) }
-    let(:expected_result) { langs.map { |lang| receiver + lang } }
-    subject { result }
-    it { should == expected_result }
-  end
-  
-  context "(1..5).map &:at.in(langs).or(default_value)" do
-    let(:default_value) { "none" }
-    let(:result) { (1..5).map &:at.in(langs).or(default_value) }
-    let(:expected_result) { (1..5).map { |i| langs.at(i) or default_value } }
-    subject { result }
-    it { should == expected_result }
+    context "langs.map &:push.to(*receivers)" do
+      let(:receivers) { Array.new(3) { [] } }
+      let(:destructed_receivers) { Array.new(3) { langs } }
+      let(:result) { langs.map &:push.to(*receivers) }
+      let(:expected_result) { langs }
+      before { result }
+
+      describe "receivers" do
+        subject { receivers }
+        it { should == destructed_receivers }
+      end
+    
+      describe "result" do
+        subject { result }
+        it { should == expected_result }
+      end
+    end
+
+    context "langs.map &:capitalize.and_push.to(*receivers)" do
+      let(:receivers) { Array.new(3) { [] } }
+      let(:destructed_receivers) { Array.new(3) { langs.map { |lang| lang.capitalize } } }
+      let(:result) { langs.map &:capitalize.and_push.to(*receivers) }
+      let(:expected_result) { langs.map &:capitalize }
+      before { result }
+    
+      describe "receivers" do
+        subject { receivers }
+        it { should == destructed_receivers }
+      end
+    
+      describe "result" do
+        subject { result }
+        it { should == expected_result }
+      end
+    end
+
+    context "langs.map &:capitalize.of_push.to(*receivers)" do
+      let(:receivers) { Array.new(3) { [] } }
+      let(:destructed_receivers) { Array.new(3) { langs } }
+      let(:result) { langs.map &:capitalize.of_push.to(*receivers) }
+      let(:expected_result) { langs.map &:capitalize }
+      before { result }
+    
+      describe "receivers" do
+        subject { receivers }
+        it { should == destructed_receivers }
+      end
+    
+      describe "result" do
+        subject { result }
+        it { should == expected_result }
+      end
+    end
   end
 
+  describe "Symbol#in" do
+
+    context "langs.map &:+.in(receiver)" do
+      let(:receiver) { "The " }
+      let(:result) { langs.map &:+.in(receiver) }
+      let(:expected_result) { langs.map { |lang| receiver + lang } }
+      subject { result }
+      it { should == expected_result }
+    end
+
+    context "langs.map &:capitalize.and(:+).in(receiver)" do
+      let(:receiver) { "The " }
+      let(:result) { langs.map &:capitalize.and(:+).in(receiver) }
+      let(:expected_result) { langs.map { |lang| receiver + lang.capitalize } }
+      subject { result }
+      it { should == expected_result }
+    end
+
+    context "langs.map &:capitalize.of(:+).in(receiver)" do
+      let(:receiver) { "The " }
+      let(:result) { langs.map &:capitalize.of(:+).in(receiver) }
+      let(:expected_result) { langs.map { |lang| (receiver + lang).capitalize } }
+      subject { result }
+      it { should == expected_result }
+    end
+  end
+  
+  describe "Symbol#or" do
+    
+    context "(0..5).map &:at.in(langs).or(default_value)" do
+      let(:default_value) { "none" }
+      let(:result) { (0..5).map &:at.in(langs).or(default_value) }
+      let(:expected_result) { (0..5).map { |i| langs.at(i) or default_value } }
+      subject { result }
+      it { should == expected_result }
+    end
+
+    context "(0..5).map &:at.in(langs).or(default_value).and_capitalize" do
+      let(:default_value) { "none" }
+      let(:result) { (0..5).map &:at.in(langs).or(default_value).and_capitalize }
+      let(:expected_result) { (0..5).map { |i| (langs.at(i) or default_value).capitalize } }
+      subject { result }
+      it { should == expected_result }
+    end
+  end
 end
--- spec/give4each/method_chain_spec.rb
+++ spec/give4each/method_chain_spec.rb
@@ -5,9 +5,10 @@ describe Give4Each::MethodChain do
   let(:to_s_method) { Give4Each::MethodChain.new :to_s }
   subject { to_s_method }
   
-  Proc.instance_methods.each do |proc_method|
-    it { should respond_to proc_method }
-  end
+  # # Maybe this is passed each time.
+  # Proc.instance_methods.each do |proc_method|
+  #   it { should respond_to proc_method }
+  # end
   
   describe "#to_proc" do
     let(:receiver) { "hello world" }
