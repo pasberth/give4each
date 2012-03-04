1a2a8587a6170e6c8119633806f70a0b9b257e83
pasberth <pasberth@gmail.com>
2012-03-04 09:01:40 +0900

    Array is avaliable as the MethodChain

--- lib/give4each/core_ext/array.rb
+++ lib/give4each/core_ext/array.rb
@@ -1,9 +1,17 @@
 class Array
+
+  def method_missing f, *a, &b # :nodoc:
+    if  Give4Each.allowing_method? f
+      Give4Each::MethodChain.new(self).send f, *a, &b
+    else
+      super
+    end
+  end
   
   def to_proc
-    lambda do |o|
+    lambda do |*a|
       map do |key|
-        key.to_proc.call o
+        key.to_proc.call *a
       end
     end
   end
